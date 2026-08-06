5fd44a72342a25cdb10e31a8a8925850bc189f23
PolSwapnil2084 <swapnilpol34@gmail.com>
2026-08-06 13:29:54 +0000

    Maths and nums done

---
+++ 01_basics/06_math_and_nums.js
@@ -0,0 +1,34 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+// console.log(typeof balance)
+
+// console.log(balance.toString().length) //=>3
+// console.log(balance.toFixed(2)) //=> 100.00
+
+const otherNum = 23.3432
+//console.log(otherNum.toPrecision(3))// give val in expo,decimal,etc
+
+const hundreds =198387598
+//console.log(hundreds.toLocaleString('en-IN')) // =>19,83,87,598
+
+
+// *=*=*=*=*=*=*=******* MATH *******=*=*=*=*==*
+
+// console.log(Math)
+// console.log(Math.abs(-8))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(3,4,2,2,4,5))
+
+// console.log(Math.random())
+// console.log(Math.random()*10)
+//console.log((Math.random()*10)+1)
+
+const min = 25
+const max = 30
+
+//console.log(Math.floor(Math.random()*(max-min+1))+min) //=>IMP  
