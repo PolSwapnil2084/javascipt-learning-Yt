cae0dd01ef68097aa8ff92c348e4de02bcbbd676
PolSwapnil2084 <swapnilpol34@gmail.com>
2026-08-12 13:48:14 +0000

    execution context

---
+++ 03_basic/05_executionContext.js
@@ -0,0 +1,14 @@
+//===========#VIMP for interview===============
+
+/* 
+JavaScript ExecutionContext -
+-{} ----> Global EC 
+-Fuctional execution context 
+-Eval execution context 
+
+{code}--->Memory creation phase 
+      --->Execution phase
+
+
+      complete at 6:08
+*/
