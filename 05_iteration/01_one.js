95f653d6ced033d24e33c0c4fb3e0cb63c16a594
PolSwapnil2084 <swapnilpol34@gmail.com>
2026-08-18 11:04:31 +0000

    iteration complete

---
+++ 05_iteration/01_one.js
@@ -0,0 +1,17 @@
+for(let i =0; i<2; i++){
+   for(let j =0;j<=2; j++){
+   //console.log(`${j}`);
+   }
+}
+// selecting same word in the code and chage it "ctrl+D"
+
+//==============Keyword=============
+//1.break and 2. continue 
+for(let i =0; i<20; i++){
+    if(i==5){
+        console.log(`Detected`)
+        continue;
+    }
+    console.log(`${i}`)
+} 
+// time line 7:49 
